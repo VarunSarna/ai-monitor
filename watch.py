55895def6c55cda640ec6aacaa8db47551317dda
VarunSarna <varun.sarna22@gmail.com>
2026-01-23 21:12:26 +0530

    Change time threshold to 1 day for scanning
    Updated time threshold to check for new repositories and papers.

--- watch.py
+++ watch.py
@@ -49,9 +49,10 @@ def check_arxiv(threshold):
 
 if __name__ == "__main__":
     # Check for anything in the last 1 hour (plus 5 min buffer)
-    time_threshold = datetime.now(timezone.utc) - timedelta(minutes=65)
+    time_threshold = datetime.now(timezone.utc) - timedelta(days=1)
     print(f"Scanning since {time_threshold}")
     
     check_github(time_threshold)
     check_arxiv(time_threshold)
     print("Scan complete.")
+
